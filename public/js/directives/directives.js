8143ba43bf6ed90b5f0851e41e59025d5977b8b4
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-09-17 11:32:33 -0400

    resized images when added to garden

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -1,5 +1,7 @@
 (function(){
   var directives = angular.module("directives", ['ngRoute']);
+
+  //  directive to create add to garden button
   directives.directive('addbutton', function(){
     return {
       restrict: 'E',
@@ -7,10 +9,12 @@
     }
   });
 
+  // directive to add plant image to garden
+  // need to extract the style (resized height/width) into another directive and call with ng-style
   directives.directive('addtogarden', function($compile){
     return function(scope, element, attrs){
       element.bind("click", function(){
-        angular.element(document.getElementById('garden-layout')).append($compile('<img class="layout-image" src=' + scope.plant.photo_url + ' />')(scope));
+        angular.element(document.getElementById('garden-layout')).append($compile('<img height='+4*scope.plant.spacing_min+' width='+4*scope.plant.spacing_min+' class="layout-image" src=' + scope.plant.photo_url + ' />')(scope));
       });
     };
   });
